f7a7c1739062297ba25b39596f52ea9a7d65fd40
lizhimin <lizhimin@localhost.localdomain>
2020-08-01 09:10:38 +0000

    github授权

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -12,6 +12,28 @@ class OauthController extends Controller
      */
     public function git()
     {
+    	//接收code
+    	$code = $_GET['code'];
+    	//换取access_token.
+    	$this->getToken($code);
     	echo '<pre>';print_r($_GET);echo '</pre>';
+    }
+    /**
+     * 根据code换取 token
+     * 
+     */
+    protected function getToken($code)
+    {
+    	$url = 'http://github.com/login/oauth/access_token';
+
+    	//post 接口 Guzzle or curl
+    	
+    	//拿到token 获取用户信息
+    	$this->getGithubUserInfo($token);
+    }
+
+    protected function getGithubUserInfo($token)
+    {
+    	$url = 'https://api.github.com/user';
     }
 }
